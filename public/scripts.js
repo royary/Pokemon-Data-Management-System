4c745b0d996e82895239272e949f02cd654e4c3c
Ruiyang Zhang <rzhan101@students.cs.ubc.ca>
2025-03-28 13:37:24 -0700

    initial server

--- public/scripts.js
+++ public/scripts.js
@@ -1,15 +1,45 @@
 
 
+const form = document.getElementById('insertDemotable');
 
-async function checkDbConnection() {
-    const statusElem = document.getElementById('dbStatus');
-    const response = await fetch('/check-db-connection', {
-        method: "GET"
-    }); 
+async function insertDemotable(event){
+    event.preventDefault();
+    const idValue = document.getElementById('insertId').value;
+    const nameValue = document.getElementById('insertName').value;
+    console.log("I'm at script.js");
+    const response = await fetch('/insert-demotable', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: idValue,
+            name: nameValue
+        })
+    });
+    console.log(response);
+
+    const responseData = await response.json();
+    const messageElement = document.getElementById('insertResultMsg');
+
+    if(responseData.success) {
+        messageElement.textContent = "Data inserted successfully";
+    }else {
+        messageElement.textContent = "Error inserting Data";
+    }
+
 }
 
 
 
-window.onload = function() {
-    checkDbConnection();
-}
+form.addEventListener("submit", insertDemotable);
+
+
+const showFormButton = document.getElementById('showFormButton');
+showFormButton.addEventListener('click', function() {
+    if(form.style.display === 'none') {
+        form.style.display = 'block';
+    } else {
+        form.style.display = 'none';
+    }
+})
