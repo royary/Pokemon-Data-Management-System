816f1173b681cb93e13ac987c79269257989bea1
Ruiyang Zhang(12568747) <rzhan101@students.cs.ubc.ca>
2025-04-02 22:17:04 -0700

    change PORT

--- remote-start.sh
+++ remote-start.sh
@@ -28,8 +28,8 @@ sed -i "/^ORACLE_HOST=/c\ORACLE_HOST=dbhost.students.cs.ubc.ca" $ENV_SERVER_PATH
 sed -i "/^ORACLE_PORT=/c\ORACLE_PORT=1522" $ENV_SERVER_PATH
 
 # Define a range
-START=50000
-END=60000
+START=60000
+END=70000
 
 # Loop through the range and check if the port is in use
 for PORT in $(seq $START $END); do
